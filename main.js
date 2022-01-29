f6b733a93cbb6ec72e3e246bdbb06efb8bf8be69
SimonSchulz <91724012+SimonSchulz@users.noreply.github.com>
2022-01-29 17:47:40 +0300

    hw lesson-10

--- main.js
+++ main.js
@@ -48,9 +48,10 @@ const data = [
         imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
     },
 ];
-const homesGuestLoves = document.getElementsByClassName('hotels_main')[0];
+let data2 =[data[0],data[1],data[2],data[3]];
 
-homesGuestLoves.innerHTML = data.map(i =>
+const homesGuestLoves = document.querySelector('.hotels_main');
+homesGuestLoves.innerHTML = data2.map(i =>
     `<div class="hotels_item">
     <div class="hotels_item_img">
     <img src="${i.imageUrl}" alt="Hotel_picture">
@@ -58,3 +59,45 @@ homesGuestLoves.innerHTML = data.map(i =>
     <div class="hotel_item_name">${i.name}</div>
     <div class="hotel_item_place">${i.city}, ${i.country}</div>
 </div>`).join('');
+console.log(homesGuestLoves)
+
+let btn_prev = document.querySelector('.left_btn_hotel');
+let btn_next = document.querySelector('.right_btn_hotel');
+
+let i=0;
+let newItem;
+
+btn_prev.onclick = function(){
+    i === 0 ? newItem= data.length-1 : newItem = i-1;
+    data2.pop();
+    data2.unshift(data[newItem]);
+    homesGuestLoves.innerHTML = data2.map(i =>
+        `<div class="hotels_item">
+    <div class="hotels_item_img">
+    <img src="${i.imageUrl}" alt="Hotel_picture">
+    </div>
+    <div class="hotel_item_name">${i.name}</div>
+    <div class="hotel_item_place">${i.city}, ${i.country}</div>
+    </div>`).join('');
+    i = i - 1;
+    if(i < 0){
+        i = data.length-1;
+    }
+}
+btn_next.onclick = function(){
+    i+4 > data.length-1 ? newItem= i+4-data.length : newItem = i+4;
+    data2.shift();
+    data2.push(data[newItem]);
+    homesGuestLoves.innerHTML = data2.map(i =>
+        `<div class="hotels_item">
+    <div class="hotels_item_img">
+    <img src="${i.imageUrl}" alt="Hotel_picture">
+    </div>
+    <div class="hotel_item_name">${i.name}</div>
+    <div class="hotel_item_place">${i.city}, ${i.country}</div>
+    </div>`).join('');
+    i = i + 1;
+    if(i === data.length){
+        i = 0;
+    }
+}
