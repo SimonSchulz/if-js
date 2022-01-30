b0515b55d57325661560df05c390c44cf4f47415
SimonSchulz <91724012+SimonSchulz@users.noreply.github.com>
2022-01-30 12:35:51 +0300

    some fixes

--- main.js
+++ main.js
@@ -49,36 +49,27 @@ const data = [
     },
 ];
 let data2 =[data[0],data[1],data[2],data[3]];
-
 const homesGuestLoves = document.querySelector('.hotels_main');
-homesGuestLoves.innerHTML = data2.map(i =>
-    `<div class="hotels_item">
-    <div class="hotels_item_img">
-    <img src="${i.imageUrl}" alt="Hotel_picture">
-    </div>
-    <div class="hotel_item_name">${i.name}</div>
-    <div class="hotel_item_place">${i.city}, ${i.country}</div>
-</div>`).join('');
-console.log(homesGuestLoves)
-
-let btn_prev = document.querySelector('.left_btn_hotel');
-let btn_next = document.querySelector('.right_btn_hotel');
-
-let i=0;
-let newItem;
-
-btn_prev.onclick = function(){
-    i === 0 ? newItem= data.length-1 : newItem = i-1;
-    data2.pop();
-    data2.unshift(data[newItem]);
-    homesGuestLoves.innerHTML = data2.map(i =>
+function drawHotelItems(){
+    return data2.map(i =>
         `<div class="hotels_item">
     <div class="hotels_item_img">
     <img src="${i.imageUrl}" alt="Hotel_picture">
     </div>
     <div class="hotel_item_name">${i.name}</div>
     <div class="hotel_item_place">${i.city}, ${i.country}</div>
-    </div>`).join('');
+</div>`).join('');
+}
+homesGuestLoves.innerHTML = drawHotelItems();
+let btn_prev = document.querySelector('.left_btn_hotel');
+let btn_next = document.querySelector('.right_btn_hotel');
+let i=0;
+let newItem;
+btn_prev.onclick = function(){
+    i === 0 ? newItem= data.length-1 : newItem = i-1;
+    data2.pop();
+    data2.unshift(data[newItem]);
+    homesGuestLoves.innerHTML = drawHotelItems();
     i = i - 1;
     if(i < 0){
         i = data.length-1;
@@ -88,14 +79,7 @@ btn_next.onclick = function(){
     i+4 > data.length-1 ? newItem= i+4-data.length : newItem = i+4;
     data2.shift();
     data2.push(data[newItem]);
-    homesGuestLoves.innerHTML = data2.map(i =>
-        `<div class="hotels_item">
-    <div class="hotels_item_img">
-    <img src="${i.imageUrl}" alt="Hotel_picture">
-    </div>
-    <div class="hotel_item_name">${i.name}</div>
-    <div class="hotel_item_place">${i.city}, ${i.country}</div>
-    </div>`).join('');
+    homesGuestLoves.innerHTML = drawHotelItems();
     i = i + 1;
     if(i === data.length){
         i = 0;
