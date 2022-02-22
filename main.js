615cd4deec0ed6f090c89d88c58e76cc3b4c289d
SimonSchulz <91724012+SimonSchulz@users.noreply.github.com>
2022-02-22 16:28:32 +0300

    hw-12.2 and 15

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // lesson-12
 let carouselItemsCount;
-if(window.innerWidth>1200){
-    carouselItemsCount=4;
-} else if (window.innerWidth<1200 && window.innerWidth>920){
-    carouselItemsCount=3
+if (window.innerWidth > 1200) {
+    carouselItemsCount = 4;
+} else if (window.innerWidth < 1200 && window.innerWidth > 920) {
+    carouselItemsCount = 3
 } else {
-    carouselItemsCount=2
+    carouselItemsCount = 2
 }
+
 // bubble sort
 function bubbleSort(arr, key) {
-    for (let i =0; i<arr.length-1;i++){
+    for (let i = 0; i < arr.length - 1; i++) {
         let swapped = false;
-        for (let j = 0; j < arr.length - 1-i; j++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
             if (arr[j][key] > arr[j + 1][key]) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -19,14 +20,18 @@ function bubbleSort(arr, key) {
                 swapped = true;
             }
         }
-        if (!swapped){return arr;}
+        if (!swapped) {
+            return arr;
+        }
     }
     return arr;
 }
-let data =[];
-const carousel =[];
+
+let data = [];
+const carousel = [];
 const list = document.querySelector('.hotels_main');
-function drawHotelItems(content){
+
+function drawHotelItems(content) {
     return content.map(i => `<div class="hotels_item">
     <div class="hotels_item_img">
     <img src="${i.imageUrl}" alt="Hotel_picture">
@@ -41,26 +46,25 @@ const url = 'https://fe-student-api.herokuapp.com/api/hotels/popular';
 const getResponse = async () => {
     try {
         const storageData = sessionStorage.getItem(url);
-        if (storageData){
+        if (storageData) {
             data = JSON.parse(storageData);
-            data = bubbleSort(data,"name");
-            for (let i = 0; i<carouselItemsCount;i++){
+            data = bubbleSort(data, "name");
+            for (let i = 0; i < carouselItemsCount; i++) {
                 carousel.push(data[i]);
             }
             list.innerHTML = drawHotelItems(carousel);
-        }
-        else {
-        const response = await fetch(url);
-        const content = await response.json();
-        sessionStorage.setItem(url,JSON.stringify(content))
-        for (let i = 0; i<content.length;i++){
-            data.push(content[i]);
-        }
-        data = bubbleSort(data, "name");
-        for (let i = 0; i<carouselItemsCount;i++){
+        } else {
+            const response = await fetch(url);
+            const content = await response.json();
+            sessionStorage.setItem(url, JSON.stringify(content))
+            for (let i = 0; i < content.length; i++) {
+                data.push(content[i]);
+            }
+            data = bubbleSort(data, "name");
+            for (let i = 0; i < carouselItemsCount; i++) {
                 carousel.push(data[i]);
-        }
-        list.innerHTML = drawHotelItems(carousel);
+            }
+            list.innerHTML = drawHotelItems(carousel);
         }
     } catch (e) {
         console.error(e);
@@ -69,25 +73,25 @@ const getResponse = async () => {
 getResponse();
 let btn_prev = document.querySelector('.left_btn_hotel');
 let btn_next = document.querySelector('.right_btn_hotel');
-let i=0;
+let i = 0;
 let newItem;
-btn_prev.onclick = function(){
-    i === 0 ? newItem= data.length-1 : newItem = i-1;
+btn_prev.onclick = function () {
+    i === 0 ? newItem = data.length - 1 : newItem = i - 1;
     carousel.pop();
     carousel.unshift(data[newItem]);
-    list.innerHTML=drawHotelItems(carousel);
+    list.innerHTML = drawHotelItems(carousel);
     i = i - 1;
-    if(i < 0){
-        i = data.length-1;
+    if (i < 0) {
+        i = data.length - 1;
     }
 }
-btn_next.onclick = function(){
-    i+carouselItemsCount > data.length-1 ? newItem= i+carouselItemsCount-data.length : newItem = i+carouselItemsCount;
+btn_next.onclick = function () {
+    i + carouselItemsCount > data.length - 1 ? newItem = i + carouselItemsCount - data.length : newItem = i + carouselItemsCount;
     carousel.shift();
     carousel.push(data[newItem]);
-    list.innerHTML=drawHotelItems(carousel);
+    list.innerHTML = drawHotelItems(carousel);
     i = i + 1;
-    if(i === data.length){
+    if (i === data.length) {
         i = 0;
     }
 }
@@ -108,8 +112,8 @@ const inputsValidation = {
         defaultValue: 0,
         counterElementId: "childrenCounter",
         additionalChanges: input => {
-            const { value } = input;
-            if ({ value })
+            const {value} = input;
+            if ({value})
                 childrenSelectors.innerHTML = "";
             for (let i = 0; i < value; i++) {
                 const newSelect = document.createElement('select');
@@ -130,16 +134,17 @@ const inputsValidation = {
 };
 
 const inputDefaultInitialization = (input) => {
-    const { min, max, defaultValue, counterElementId, additionalChanges } = inputsValidation[input.name];
+    const {min, max, defaultValue, counterElementId, additionalChanges} = inputsValidation[input.name];
     input.value = defaultValue;
     const ElChang = () => {
         counterElement.textContent = input.value;
         if (additionalChanges) {
             additionalChanges(input);
-        }}
+        }
+    }
     const counterElement = document.getElementById(counterElementId);
     counterElement.textContent = defaultValue;
-    const btns = Array.prototype.slice.call( input.closest(".numberChanger").getElementsByTagName("button") );
+    const btns = Array.prototype.slice.call(input.closest(".numberChanger").getElementsByTagName("button"));
     for (let btn of btns) {
         if (btn.textContent === "-") {
             btn.addEventListener('click', () => {
@@ -148,7 +153,7 @@ const inputDefaultInitialization = (input) => {
                     input.value--;
                     plusBtn.classList.add("btnActive");
                 }
-                if(input.value <= min) {
+                if (input.value <= min) {
                     btn.classList.remove("btnActive");
                 }
                 ElChang()
@@ -160,7 +165,7 @@ const inputDefaultInitialization = (input) => {
                     input.value++;
                     minusBtn.classList.add("btnActive");
                 }
-                if(input.value >= max) {
+                if (input.value >= max) {
                     btn.classList.remove("btnActive");
                 }
                 ElChang()
@@ -174,17 +179,17 @@ for (let input of inputs) {
 }
 const mainMenu = document.querySelector(".header_form_people");
 const menu = document.querySelector('.modalMenu');
-const toggleMenu = function() {
+const toggleMenu = function () {
     menu.classList.toggle('open');
     mainMenu.classList.toggle('third__inputActive')
 }
 
-mainMenu.addEventListener('click', function(e) {
+mainMenu.addEventListener('click', function (e) {
     e.stopPropagation();
     menu.classList.toggle('open');
     mainMenu.classList.toggle('third__inputActive')
 });
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     const target = e.target;
     const its_menu = target === menu || menu.contains(target);
     const its_mainMenu = target === mainMenu;
@@ -193,3 +198,48 @@ document.addEventListener('click', function(e) {
         toggleMenu();
     }
 });
+
+//lesson 12.2
+const getChildrenSelectorValues = () => {
+    const selectors = childrenSelectors.getElementsByTagName("select");
+    const childAgesData = [];
+    for (let selector of selectors) {
+        childAgesData.push(selector.value);
+    }
+    return childAgesData.toString();
+}
+const availableBlock = document.querySelector('.available_hotels');
+const searchFormBtn = document.querySelector('.form_btn');
+const submitForm = (event) => {
+    event.preventDefault();
+    const textToFind = document.getElementById('form_destinations').value;
+    const adults = event.target.adults;
+    const children = getChildrenSelectorValues();
+    const rooms = event.target.rooms;
+    fetch(`https://fe-student-api.herokuapp.com/api/hotels?search=${textToFind}&adults=${adults}&children=${children}&rooms=${rooms}`)
+        .then(data => data.text())
+        .then(data => {
+            return JSON.parse(data);
+        })
+        .then(data => {
+            const availableForm = document.querySelector(".available_wrapper");
+            availableForm.innerHTML = data.map(i => `
+        <div class="hotels_item">
+        <div class="available_img">
+        <img src="${i.imageUrl}" alt="Hotel_picture">
+        </div>
+         <div class="available_name">${i.name}</div>
+        <div class="available_place">${i.city}, ${i.country}</div>
+        </div>`).join('');
+        })
+        .then(data => {
+            availableBlock.classList.remove('none');
+        })
+}
+searchFormBtn.addEventListener('click', submitForm);
+
+
+
+
+
+
